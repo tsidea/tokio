b28fc1e9241bcf9fbe5141430989c1442438c106
github-action-benchmark <github@users.noreply.github.com>
2021-01-13 15:58:21 +0000

    add sync_rwlock (cargo) benchmark result for 766a89bf94631da0d30d43e962f35ee623b9f1d8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610553491853,
+  "lastUpdate": 1610553501859,
   "repoUrl": "https://github.com/tsidea/tokio",
   "entries": {
     "rt_multi_threaded": [
@@ -47,6 +47,62 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 21147978,
             "range": "± 3380773",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "sync_rwlock": [
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "766a89bf94631da0d30d43e962f35ee623b9f1d8",
+          "message": "chore: prepare tokio-stream 0.1.2 (#3414)",
+          "timestamp": "2021-01-12T21:38:51+01:00",
+          "tree_id": "567616b1b295c7e0f49dd48d4414f83d38452588",
+          "url": "https://github.com/tsidea/tokio/commit/766a89bf94631da0d30d43e962f35ee623b9f1d8"
+        },
+        "date": 1610553500541,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "read_concurrent_contended",
+            "value": 714,
+            "range": "± 259",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_contended_multi",
+            "value": 13038,
+            "range": "± 4346",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended",
+            "value": 749,
+            "range": "± 211",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended_multi",
+            "value": 13275,
+            "range": "± 4268",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_uncontended",
+            "value": 420,
+            "range": "± 164",
             "unit": "ns/iter"
           }
         ]
